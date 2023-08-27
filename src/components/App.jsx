e940538d501940fd10fe609d46e92223705d697a
Vika14 <manunya1406@gmail.com>
2023-08-27 19:56:26 +0200

    hw2-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm';
+import { Filter } from './Filter';
+import { Contacts } from './Contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  formSubmitHandler = e => {
+    const id = nanoid();
+    const name = e.name;
+    const number = e.number;
+    const contacts = [...this.state.contacts];
+
+    if (contacts.findIndex(contact => name === contact.name) !== -1) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      contacts.push({ name, id, number });
+    }
+    this.setState({ contacts: contacts });
+  };
+
+  handleChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  handleDelete = e => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== e),
+    }));
+  };
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} handleChange={this.handleChange} />
+        <Contacts
+          contacts={this.filterContacts()}
+          handleDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from './app.module.css';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  nameInputId = nanoid();
+  numberInputId = nanoid();
+
+  handleChange = e => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className={css.forma}>
+        <label htmlFor={this.nameInputId} className={css.nameNumber}>
+          Name{' '}
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+            id={this.nameInputId}
+            className={css.pole}
+          />
+        </label>
+        <label htmlFor={this.numberInputId} className={css.nameNumber}>
+          Number{' '}
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+            id={this.numberInputId}
+            className={css.pole}
+          />
+        </label>
+
+        <button type="submit" className={css.btn}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
